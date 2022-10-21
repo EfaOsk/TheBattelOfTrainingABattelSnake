284cf4d14a93f9d922d7ce266a9c4b9bed88e20b
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2022-10-21 09:37:01 +0000

    stay inside the board

--- main.py
+++ main.py
@@ -63,8 +63,17 @@ def move(game_state: typing.Dict) -> typing.Dict:
         is_move_safe["up"] = False
 
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
+    board_width = game_state['board']['width']
+    board_height = game_state['board']['height']
+    if my_head["x"] + 1 == board_width:
+        is_move_safe["right"] = False
+    elif my_head["x"] == 0:
+        is_move_safe["left"] = False
+    if my_head["y"] + 1 == board_height:
+        is_move_safe["up"] = False
+    elif my_head["y"] == 0:
+        is_move_safe["down"] = False
+    
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     # my_body = game_state['you']['body']
