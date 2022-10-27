ee5fae717c052daf3af483a3c91dd4df576ec560
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2022-10-27 11:45:09 +0000

    Increased the batch size
    Increased the batch size from 1 to 128

--- model.py
+++ model.py
@@ -66,7 +66,7 @@ summary(model,(1, 1, 11, 11))
 
 
 # Train
-BATCH_SIZE = 1
+BATCH_SIZE = 128
 GAMMA = 0.999
 EPS_START = 0.9
 EPS_END = 0.05
@@ -175,45 +175,3 @@ def optimize_model():
         param.grad.data.clamp_(-1, 1)
     optimizer.step()
 
-
-# num_episodes = 50
-# for i_episode in range(num_episodes):
-#     # Initialize the environment and state
-#     env.reset()
-#     last_screen = get_screen()
-#     current_screen = get_screen()
-#     state = current_screen - last_screen
-#     for t in count():
-#         # Select and perform an action
-#         action = select_action(state)
-#         _, reward, done, _, _ = env.step(action.item())
-#         reward = torch.tensor([reward], device=device)
-
-#         # Observe new state
-#         last_screen = current_screen
-#         current_screen = get_screen()
-#         if not done:
-#             next_state = current_screen - last_screen
-#         else:
-#             next_state = None
-
-#         # Store the transition in memory
-#         memory.push(state, action, next_state, reward)
-
-#         # Move to the next state
-#         state = next_state
-
-#         # Perform one step of the optimization (on the policy network)
-#         optimize_model()
-#         if done:
-#             episode_durations.append(t + 1)
-#             plot_durations()
-#             break
-#     # Update the target network, copying all weights and biases in DQN
-#     if i_episode % TARGET_UPDATE == 0:
-#         target_net.load_state_dict(policy_net.state_dict())
-
-# print('Complete')
-
-# plt.ioff()
-# plt.show()
